82a2a6091bcc6de6356b60da89cf87b71a366f8b
chindden <34678156+chindden@users.noreply.github.com>
2025-01-04 17:17:03 +0800

    Update check_sb_cron.sh

--- check_sb_cron.sh
+++ check_sb_cron.sh
@@ -1,8 +1,7 @@
-```bash
 #!/bin/bash
 
-#red() { echo -e "\e[1;91m$1\033[0m"; }
-#green() { echo -e "\e[1;32m$1\033[0m"; }
+red() { echo -e "\e[1;91m$1\033[0m"; }
+green() { echo -e "\e[1;32m$1\033[0m"; }
 USERNAME=$(whoami)
 WORKDIR="/home/${USERNAME}/logs"
 CRON_NEZHA="nohup ./nezha.sh >/dev/null 2>&1 &"
@@ -11,21 +10,20 @@ CRON_ARGO="nohup ./argo.sh >/dev/null 2>&1 &"
 chmod -R 755 "${WORKDIR}"
 
 (crontab -l | grep -v -E "@reboot pkill -kill -u $(whoami)|pgrep -x \"npm\"|pgrep -x \"web\"|pgrep -x \"bot\"") | crontab -
-red "检查已存在的特定任务并清除"
+echo "检查已存在的特定任务并清除"
 crontab -r
-#red "清除所有已存在的 crontab 任务"
+#echo "清除所有已存在的 crontab 任务"
 
 # 初始化一个新的 crontab 文件内容
 NEW_CRONTAB=""
 
-green "正在添加 保活任务 的 crontab 重启任务"
+echo "正在添加 保活任务 的 crontab 重启任务"
 NEW_CRONTAB+="@reboot pkill -kill -u $(whoami) cd ${WORKDIR} && ${CRON_SB}\n"
-NEW_CRONTAB+="* * * * * curl -s https://raw.githubusercontent.com/chindden/serv00-vmess-sock5/refs/heads/main/check_sb_cron.sh -o check_sb_cron.sh && bash check_sb_cron.sh\n"
-NEW_CRONTAB+="*/2 * * * * if ! ps aux | grep '[c]onfig' > /dev/null || ! ps aux | grep [l]ocalhost > /dev/null; then /bin/bash domains/${USERNAME}.serv00.net/logs/serv00keep.sh; fi\n"
+NEW_CRONTAB+="* * * * * curl -s https://raw.githubusercontent.com/qmsdh.com/serv00-ct8-ssh/refs/heads/main/check_sb_cron.sh -o check_sb_cron.sh && bash check_sb_cron.sh\n"
+NEW_CRONTAB+="*/2 * * * * if ! ps aux | grep '[c]onfig' > /dev/null || ! ps aux | grep [l]ocalhost > /dev/null; then /bin/bash domains/chindeen.serv00.net/logs/serv00keep.sh; fi\n"
 
 
 
 # 将 crontab 任务更新一次性添加
 (crontab -l; echo -e "$NEW_CRONTAB") | crontab -
-green "Crontab 任务已添加完成"
-```
+echo "Crontab 任务已添加完成"
